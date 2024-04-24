f2aeacfb92c5425d5c7977d999576f4a876a870d
muhammadarsalanmanzoor <155964619+muhammadarsalanmanzoor@users.noreply.github.com>
2024-04-24 11:46:45 +0500

    implemented security packages

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// ***** extra security packages *****
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 // ***** routers *****
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
@@ -16,8 +22,20 @@ const authenticationMiddleware = require('./middleware/authentication');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+// ***** middleware for accessing req.body inside our controller *****
 app.use(express.json());
+
 // ***** extra packages *****
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // ***** 15 minutes
+    max: 100,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // ***** routes *****
 app.use('/api/v1/auth', authRouter);
